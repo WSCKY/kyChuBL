f6a6c2f718c96eecbb4d4a3bcc9390e20f768baa
kyChu <hello.kychu@gmail.com>
2018-01-22 21:50:50 +0800

    add bootloader global configuration file

---
+++ inc/BootloaderConfig.h
@@ -0,0 +1,8 @@
+#ifndef __BOOTLOADERCONFIG_H
+#define __BOOTLOADERCONFIG_H
+
+#include "stm32f7xx.h"
+
+#define APPLICATION_START_ADDRESS                (0x08008000)
+
+#endif /* __BOOTLOADERCONFIG_H */
