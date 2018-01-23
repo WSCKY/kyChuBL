3522e2f62a6d9d9a6cfee29a5e0f7c843572723e
kyChu <hello.kychu@gmail.com>
2018-01-23 13:24:29 +0800

    code clean up && update readme.txt

--- inc/FAT_TAB.h
+++ inc/FAT_TAB.h
@@ -1,15 +1,19 @@
-#include "stm32f7xx_hal.h"
+/**
+  ******************************************************************************
+  * @file    FAT_TAB.h
+  * @author  kyChu
+  * @version V1.0
+  * @date    23-January-2018
+  * @brief   FAT12 file system data structure.
+  ******************************************************************************
+*/
 
-#define __LOBYTE(x)                    ((uint8_t)((x) & 0x00FF))
-#define __HIBYTE(x)                    ((uint8_t)(((x) & 0xFF00) >>8))
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __FAT_TAB_H
+#define __FAT_TAB_H
 
-#define FILE_DATE(y, m, d)             (((((uint16_t)(y) - 1980) & 0x7F) << 9) | \
-                                                 (((uint16_t)(m) & 0x0F) << 5) | \
-																			           (((uint16_t)(d) & 0x1F) << 0))
-
-#define FILE_TIME(h, m, s)             ((((uint16_t)(h) & 0x1F) << 11) | \
-                                         (((uint16_t)(m) & 0x3F) << 5) | \
-																	       (((uint16_t)(s >> 1) & 0x1F) << 0))
+/* Includes ------------------------------------------------------------------*/
+#include "BootloaderMaths.h"
 
 /* File Attributes --------------------------------------------------------- */
 #define FILE_ATTRIBUTES_RWE            0x00      /* Read Write */
@@ -25,6 +29,7 @@
 /* FATFS ERROR ------------------------------------------------------------- */
 #define FATFS_OK                       (0)
 #define FATFS_ERROR                    (-1)
+
 /* FATFS DUMMY BYTE -------------------------------------------------------- */
 #define RESERVED_BYTE                  ((uint8_t)0x00)
 
@@ -41,12 +46,25 @@
 
 #define TOTAL_SECTORS                  (BYTES_CONV_SECTORS(FATFS_USED_SIZE + FLASH_USER_SIZE))
 
+/* Readme.txt file data ---------------------------------------------------- */
+#define README_FILE_LEN                301
+static const unsigned char README_DATA[README_FILE_LEN] =
+"kyChu FC Automatically-generated file. Do not edit!\r\n"
+"Boot Version: V1.0.0\r\n"
+"Flight Control Version: V1.0.0\r\n"
+"Drone Serial Number: KY1801230001V1CN\r\n"
+"\r\n""\r\n""\r\n"
+"We choose to go to the moon in this decade and do the other things,\r\n"
+"not because they are easy, but because they are hard.\r\n"
+" -- John F. Kennedy, 1962";
+/* Readme.txt file data ---------------------------------------------------- */
+
 /* ---------- FATFS BOOT TABLE ---------- */
-#define BOOT_TABLE_SIZE                SECTOR_SIZE
-#define BOOT_TABLE_SECTOR_NUM          (1)
-#define BOOT_TABLE_SECTOR_IDX          (0)
-#define BOOT_TABLE_OFFSET              (0)
-#define BOOT_TABLE_USED_SIZE           (62)
+#define BOOT_TABLE_SIZE                SECTOR_SIZE         /* table size */
+#define BOOT_TABLE_SECTOR_NUM          (1)                 /* number of sector used */
+#define BOOT_TABLE_SECTOR_IDX          (0)                 /* sector offset */
+#define BOOT_TABLE_OFFSET              (0)                 /* address offset */
+#define BOOT_TABLE_USED_SIZE           (62)                /* number of bytes used */
 static uint8_t BOOT_TABLE[62] = { /* MSWIN4.1 */
 0xEB, 0x3C, 0x90,  'k',  'y',  'C',  'h',  'u',  '^',  '_',  '^', __LOBYTE(SECTOR_SIZE), __HIBYTE(SECTOR_SIZE), 0x01, 0x01, 0x00, 
 0x02, 0x20, 0x00, __LOBYTE(TOTAL_SECTORS), __HIBYTE(TOTAL_SECTORS), 0xF0, 0x03, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 
@@ -55,19 +73,19 @@ static uint8_t BOOT_TABLE[62] = { /* MSWIN4.1 */
 };
 
 /* ---------- FATFS FATn TABLE ---------- */
-#define FATn_TABLE_SIZE                (SECTOR_SIZE * 3)
-#define FATn_TABLE_SECTOR_NUM          (3)
-#define FAT1_TABLE_SECTOR_IDX          (1)
-#define FAT1_TABLE_OFFSET              ((uint32_t)0x200)
-#define FAT2_TABLE_SECTOR_IDX          (4)
-#define FAT2_TABLE_OFFSET              ((uint32_t)0x800)
+#define FATn_TABLE_SIZE                (SECTOR_SIZE * 3)   /* table size */
+#define FATn_TABLE_SECTOR_NUM          (3)                 /* number of sector used */
+#define FAT1_TABLE_SECTOR_IDX          (1)                 /* sector offset */
+#define FAT1_TABLE_OFFSET              ((uint32_t)0x200)   /* address offset */
+#define FAT2_TABLE_SECTOR_IDX          (4)                 /* sector offset */
+#define FAT2_TABLE_OFFSET              ((uint32_t)0x800)   /* address offset */
 static uint8_t FATn_TABLE[FATn_TABLE_SIZE] = {0x00, 0x00, 0x00, 0x03, 0x40, 0x00, 0xFF, 0x0F};
 
 /* ---------- FATFS ROOT TABLE ---------- */
-#define ROOT_TABLE_SIZE                (SECTOR_SIZE * 2)
-#define ROOT_TABLE_SECTOR_NUM          (2)
-#define ROOT_TABLE_SECTOR_IDX          (7)
-#define ROOT_TABLE_OFFSET              ((uint32_t)0xE00)
+#define ROOT_TABLE_SIZE                (SECTOR_SIZE * 2)   /* table size */
+#define ROOT_TABLE_SECTOR_NUM          (2)                 /* number of sector used */
+#define ROOT_TABLE_SECTOR_IDX          (7)                 /* sector offset */
+#define ROOT_TABLE_OFFSET              ((uint32_t)0xE00)   /* address offset */
 static uint8_t ROOT_TABLE[ROOT_TABLE_SIZE] = {
 	/* TAB - 1 */
 	 'k',  'y',  '.',  'C',  'h',  'u',  ' ',  'D',  'F',  'U',  ' ', FILE_ATTRIBUTES_VOL, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, 
@@ -79,31 +97,16 @@ static uint8_t ROOT_TABLE[ROOT_TABLE_SIZE] = {
 //0x9B, 0x47, 0x9B, 0x47, 0x00, 0x00, 0xB1, 0xA6, 0x9B, 0x47, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 	/* TAB - 3 */
 	 'R',  'E',  'A',  'D',  'M',  'E',  ' ',  ' ',  'T',  'X',  'T', FILE_ATTRIBUTES_RDO, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, 
-	RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, __LOBYTE(FILE_TIME(22, 45, 32)), __HIBYTE(FILE_TIME(22, 45, 32)), __LOBYTE(FILE_DATE(1993, 8, 6)), __HIBYTE(FILE_DATE(1993, 8, 6)), 0x02, 0x00, 0x14, 0x05, 0x00, 0x00,
+	RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, RESERVED_BYTE, __LOBYTE(FILE_TIME(22, 45, 32)), __HIBYTE(FILE_TIME(22, 45, 32)), __LOBYTE(FILE_DATE(1993, 8, 6)), __HIBYTE(FILE_DATE(1993, 8, 6)), 0x02, 0x00,
+	_FILE_SIZE_BYTE1_(README_FILE_LEN), _FILE_SIZE_BYTE2_(README_FILE_LEN), _FILE_SIZE_BYTE3_(README_FILE_LEN), _FILE_SIZE_BYTE4_(README_FILE_LEN),
 };
 
-#define README_FILE_LEN                1300
+/* ---------- README FILE DATA ---------- */
 #define README_SECT_IDX                FATFS_TOTAL_SECTORS
 #define README_SECT_NUM                (BYTES_CONV_SECTORS(README_FILE_LEN) + 1)
 
 #define README_TAIL_LEN                (README_FILE_LEN - SECTORS_CONV_BYTES(BYTES_CONV_SECTORS(README_FILE_LEN)))
-//const
-static unsigned char README_DATA[README_FILE_LEN] =
-"This is the first of a pair of papers on the theory and implementation of a\r\n"
-"direction-cosine-matrix (DCM) based inertial measurement unit for application\r\n"
-"in model planes and helicopters. Actually, at this point, it is still a draft, there\r\n"
-"is still a lot more work to be done. Several reviewers, especially Louis\r\n"
-"LeGrand and UFO-man, have made good suggestions on additions and\r\n"
-"revisions that we should make and prepared some figures that we have not\r\n"
-"included yet. We will eventually incorporate their suggestions, but it may take\r\n"
-"a long time to get there. In the meantime, we think there is an audience who\r\n"
-"can benefit from what we have so far.\r\n"
-"The motivation for DCM was to take the next step in stabilization and\r\n"
-"control functions from an inherently stable aircraft with elevator and rudder\r\n"
-"control, to an aerobatic aircraft with ailerons and elevator. One of the authors\r\n"
-"(Premerlani) built a two axes board several years ago, and developed\r\n"
-"rudimentary firmware to provide stabilization and return-to-launch (RTL)\r\n"
-"functions for a Gentle Lady sailplane. The firmware worked well enough, and\r\n"
-"the author came to rely on the RTL feature, but it never seemed to work as\r\n"
-"well as the author would like. In particular, satisfactory solutions to the\r\n"
-"following two issues were never found:  ";
+
+#endif /* __FAT_TAB_H */
+
+/* ------------------------ (C) COPYRIGHT kyChu ----------- END OF FILE ----- */
