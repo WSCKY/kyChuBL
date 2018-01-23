d35e26b24cc91b3e9bdd9e57c4bd9e9bab4369a6
kyChu <hello.kychu@gmail.com>
2018-01-23 09:51:36 +0800

    add debug enable definition && DEBUG macro

--- inc/BootloaderConfig.h
+++ inc/BootloaderConfig.h
@@ -3,6 +3,16 @@
 
 #include "stm32f7xx.h"
 
+/* User Application start address */
 #define APPLICATION_START_ADDRESS                (0x08008000)
+
+/* Debug enable */
+#define DEBUG_LOG_ENABLE                         (0)
+#if (DEBUG_LOG_ENABLE)
+  #define DEBUG(...)                             printf(__VA_ARGS__); \
+                                                 printf("\n");
+#else
+  #define DEBUG(...)
+#endif /* DEBUG_LOG_ENABLE */
 
 #endif /* __BOOTLOADERCONFIG_H */
