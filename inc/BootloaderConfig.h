0ce59c1c7e270b17ed899034154a76f1436b09f2
kyChu <hello.kychu@gmail.com>
2018-01-23 13:22:39 +0800

    add code annotation

--- inc/BootloaderConfig.h
+++ inc/BootloaderConfig.h
@@ -1,11 +1,28 @@
+/**
+  ******************************************************************************
+  * @file    BootloaderConfig.h
+  * @author  kyChu
+  * @version V1.0
+  * @date    23-January-2018
+  * @brief   Boot loader global configuration.
+  ******************************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __BOOTLOADERCONFIG_H
 #define __BOOTLOADERCONFIG_H
 
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f7xx.h"
 
+/* Exported types ------------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported definitions ------------------------------------------------------*/
 /* User Application start address */
 #define APPLICATION_START_ADDRESS                (0x08008000)
 
+/* Exported macros -----------------------------------------------------------*/
 /* Debug enable */
 #define DEBUG_LOG_ENABLE                         (0)
 #if (DEBUG_LOG_ENABLE)
@@ -16,3 +33,5 @@
 #endif /* DEBUG_LOG_ENABLE */
 
 #endif /* __BOOTLOADERCONFIG_H */
+
+/* ------------------------ (C) COPYRIGHT kyChu ----------- END OF FILE ----- */
